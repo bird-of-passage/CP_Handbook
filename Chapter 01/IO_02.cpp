2ffa902fe35741101fb3192f0fc3443c44393661
Invincible-21 <shantobd1969@gmail.com>
2022-08-26 10:47:25 +0600

    initial commit

---
+++ Chapter 01/IO_02.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //solution comes here
+
+    int a = 123, b = 456;
+    printf("%d %d\n",a,b);
+    
+
+    return 0;
+}
